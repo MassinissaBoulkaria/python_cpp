810c4948e1df8c706c758fe1db3929adf5b2b13f
MassinissaBoulkaria <massinissa.boulkaria@dauphine.eu>
2020-03-23 15:40:11 +0100

    Create hash.h

---
+++ sha3/hash.h
@@ -0,0 +1,12 @@
+  GNU nano 2.9.3                                                                            hash.h                                                                                      
+
+#pragma once
+#ifndef HASH_H
+#define HASH_H
+
+#include <string>
+
+
+std::string hash(std::string input);
+
+#endif
